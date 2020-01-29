48716b09b2923fa6d36aa414d7b367032c48a110
Ash Yveth Cudiamat <acudiamat@nevada.unr.edu>
2020-01-28 16:51:25 -0800

    current version

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import tictactoe
+from tictactoe import Board
 
-if __name__ == '__main__':
-    board = Board()
-    winner = board.play_game()
-    print("{} has won!".format(winner))
+board = Board()
+board.mark_square(0,0,'X')
+
+#if __name__ == '__main__':
+#    board = Board()
+    #winner = board.play_game()
+    #print("{} has won!".format(winner))
--- tictactoe.py
+++ tictactoe.py
@@ -2,7 +2,7 @@
           and you make use of relevant object-oriented design principles.
 """
 
-class Board():
+class Board:
 
     tictactoeBoard = []
     
@@ -10,8 +10,17 @@ class Board():
         """
         Initializes the Board of size 3x3
         """
+        # 0-8
+        # 0 - empty space, 1 - x's, 2 - o's
+        for x in range(8):
+            self.tictactoeBoard.append('-')
 
-        pass
+    def valid_input(self, column,row):
+        input = (row*3) + column
+        if(input >= 0 and input <= 8):
+            return True
+        else:
+            return False
 
     def mark_square(self, column, row, player):
         """
@@ -23,16 +32,36 @@ class Board():
 
         :return: ????
         """
-
-        pass
-
+        if(self.valid_input):
+            input = (row*3) + column
+            self.tictactoeBoard[input] = player.lower()
+            self.display_board()
+            return True
+        else:
+            return False
+        
     def has_winner(self):
         """
         Checks to see if there is a current winner of the game
 
         :return: ????
         """
+        # Check Columns 0-2
+        for columns in range(2):
+            if(tictactoeBoard[columns] == tictactoeBoard[columns+3] and
+               tictactoeBoard[columns+3] == tictactoeBoard[columns+6]):
+                return True
 
+        # Check Rows..
+        '''
+        for rows in range(2):
+            if(tictactoeBoard[] == tictactoeBoard[+1] and
+               tictactoeBoard[columns+1] == tictactoeBoard[columns+2]):
+        '''
+
+        # Check Diagonals
+
+        
         pass
 
     def play_game(self):
@@ -44,5 +73,18 @@ class Board():
         """
         
         pass
+
+    def display_board(self):
+       ''' 
+for i in range(2):
+            for x in range(2):
+                print(self.tictactoeBoard[(x*3)+i])
+            print()
+'''
+       for i in range(8):
+           print(i)
+           print(self.tictactoeBoard[i])
+       
+            
         
 
